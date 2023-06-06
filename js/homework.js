8879f212706c1d16e586078b054dd6da6d6856e7
sdfghjkouygfv <zvezdnyjmalciska@gmail.com>
2023-06-06 17:05:08 +0500

    first js code

---
+++ js/homework.js
@@ -0,0 +1,107 @@
+// ДЗ
+
+
+// let obj = {
+//     // key = value
+//     name: "Samir",
+//     secondname: "Tashmatov",
+//     gender: "Man",
+//     DOB: "24 June 2012",
+//     age: 10,
+//     country: "Uzbekistan",
+//     city: "Tashkent",
+//     number: "+998946563050",
+//     role: "Student",
+//     grade: 6,
+// }
+
+// console.log(Object.values(obj));
+// console.log(Object.keys(obj));
+
+// let array = [
+//     35053, "стринг", 234.5463, true, false
+// ]
+
+// console.log(Object.values(obj));
+// console.log(Object.keys(obj));
+
+
+// let result = Math.trunc(10.231);
+// let result = Math.ceil(10.231);
+// let result = Math.round(10.231);
+// let result = Math.floor(10.231);
+// let result = Math.pow(10, 231);
+// let result = Math.sqrt(301);
+// let result = Math.abs(-430);
+// let result = Math.PI;
+// let result = Math.max(131, 634, 75, 685)
+// let result = Math.mini(153, 212, 422, 354);
+// let result = Math.split(100 200);
+// let result = Math.random(532);
+
+// console.log(result);
+// console.log(result * 32);
+
+// let result = Math.trunc(10.231);
+// let result = Math.ceil(10.231);
+// let result = Math.round(10.231);
+// let result = Math.floor(10.231);
+// let result = Math.pow(10, 231);
+// let result = Math.sqrt(301);
+// let result = Math.abs(-430);
+// let result = Math.PI;
+// let result = Math.max(131, 634, 75, 685)
+// let result = Math.mini(153, 212, 422, 354);
+// let result = Math.split(100 200);
+// let result = Math.random(532);
+
+// console.log(result);
+// console.log(result * 32);
+
+// let result = Math.trunc(10.231);
+// let result = Math.ceil(10.231);
+// let result = Math.round(10.231);
+// let result = Math.floor(10.231);
+// let result = Math.pow(10, 231);
+// let result = Math.sqrt(301);
+// let result = Math.abs(-430);
+// let result = Math.PI;
+// let result = Math.max(131, 634, 75, 685)
+// let result = Math.mini(153, 212, 422, 354);
+// let result = Math.split(100 200);
+// let result = Math.random(532);
+
+// console.log(result);
+// console.log(result * 32);
+
+// let result = Math.trunc(10.231);
+// let result = Math.ceil(10.231);
+// let result = Math.round(10.231);
+// let result = Math.floor(10.231);
+// let result = Math.pow(10, 231);
+// let result = Math.sqrt(301);
+// let result = Math.abs(-430);
+// let result = Math.PI;
+// let result = Math.max(131, 634, 75, 685)
+// let result = Math.mini(153, 212, 422, 354);
+// let result = Math.split(100 200);
+// let result = Math.random(532);
+
+// console.log(result);
+// console.log(result * 32);
+
+// let result = Math.trunc(10.231);
+// let result = Math.ceil(10.231);
+// let result = Math.round(10.231);
+// let result = Math.floor(10.231);
+// let result = Math.pow(10, 231);
+// let result = Math.sqrt(301);
+// let result = Math.abs(-430);
+// let result = Math.PI;
+// let result = Math.max(131, 634, 75, 685)
+// let result = Math.mini(153, 212, 422, 354);
+// let result = Math.split(100 200);
+// let result = Math.random(532);
+
+// console.log(result);
+// console.log(result * 32);
---
+++ js/main.js
@@ -0,0 +1,65 @@
+// library
+
+// let text = prompt("hello pupsik");
+// alert("hello vasya");
+// console.log(text);
+
+// variables -> let, const, var
+
+// let number1 = 10;
+// let number2 = 100;
+
+// console.log(number1 + number2);
+
+
+//  data types
+// String, Boolean, Number, isNaN, BigInt, Symbol, Array, Object;
+// let string = "sdvzfbfg";
+// console.log(string);
+// Boolean
+// true, false
+// isNaN -> is Not a Nubmer
+// console.log(isNaN(number1));
+
+// bigInt
+// 5,26561
+// Symbol
+// && || !
+
+// console.log(!true);
+// let str1 = "a";
+// let str2 = "b";
+// console.log(str1 + str2);
+
+// let numberOne = 20;
+// let numberTwo = "30";
+// let result = numberOne number Two;
+// let result = numberOne + numberTwo;
+// let result = numberOne / numberTwo;
+// let result = numberOne * numberTwo;
+// console.log(result);
+// + переводит стринг тайп в наймбер
+// console.log(typeof +string);
+
+// let array = [1, "str", 2.6452, true, false,];
+
+// let obj = {
+//     // key: value
+//     name: "Samir",
+//     age: 10,
+//     address: "Sholohova str",
+// }
+
+// // console.log(Object.keys(obj)); элементы
+// console.log(Object.values(obj));
+
+// let education_center = {
+//     subjects: ["math", "frontend", "english"],
+//     cost: [35000, 45000, 350]
+// }
+
+// console.log(education_center.values(obj));
+
+// let result = Math.trunc(num1)(2.1)
+
+// console.log(result);
